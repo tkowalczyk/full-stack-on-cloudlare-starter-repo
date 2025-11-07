60a178f10f5d30aba1909291cf59d09bc9aac05c
Tomasz Kowalczyk <tomek@kownet.info>
2025-11-07 18:03:05 +0100

    protecting be routes

--- apps/user-application/worker/hono/app.ts
+++ apps/user-application/worker/hono/app.ts
@@ -3,30 +3,48 @@ import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/worker/trpc/router";
 import { createContext } from "@/worker/trpc/context";
 import { getAuth } from "@repo/data-ops/auth";
+import { createMiddleware } from "hono/factory";
 
-export const App = new Hono<{ Bindings: ServiceBindings }>();
+export const App = new Hono<{ Bindings: ServiceBindings, Variables: { userId: string } }>();
 
-App.all("/trpc/*", (c) => {
+const getAuthInstance = (env: Env) => {
+  return getAuth({
+    clientId: env.GOOGLE_CLIENT_ID,
+    clientSecret: env.GOOGLE_CLIENT_SECRET
+  })
+}
+
+const authMiddleware = createMiddleware(async (c, next) => {
+  const auth = getAuthInstance(c.env)
+  const session = await auth.api.getSession({ headers: c.req.raw.headers });
+  if (!session?.user) {
+    return c.text("Unauthorized", 401);
+  }
+  const userId = session.user.id;
+  c.set("userId", userId);
+  await next()
+})
+
+App.all("/trpc/*", authMiddleware, (c) => {
+  const userId = c.get("userId")
   return fetchRequestHandler({
     endpoint: "/trpc",
     req: c.req.raw,
     router: appRouter,
     createContext: () =>
-      createContext({ req: c.req.raw, env: c.env, workerCtx: c.executionCtx }),
+      createContext({ req: c.req.raw, env: c.env, workerCtx: c.executionCtx, userId }),
   });
 });
 
-App.get("/click-socket", async (c) => {
+App.get("/click-socket", authMiddleware, async (c) => {
+  const userId = c.get("userId")
   const headers = new Headers(c.req.raw.headers);
-  headers.set("account-id", "1234567890");
+  headers.set("account-id", userId);
   const proxiedRequest = new Request(c.req.raw, { headers });
   return c.env.BACKEND_SERVICE.fetch(proxiedRequest);
 });
 
 App.on(["POST", "GET"], "/api/auth/*", (c) => {
-  const auth = getAuth({
-    clientId: c.env.GOOGLE_CLIENT_ID,
-    clientSecret: c.env.GOOGLE_CLIENT_SECRET
-  })
-	return auth.handler(c.req.raw);
+  const auth = getAuthInstance(c.env);
+  return auth.handler(c.req.raw);
 });
