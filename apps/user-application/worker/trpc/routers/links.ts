19dc0e79f8aa37b7da83bf827ec64eefbd75955b
Tomasz Kowalczyk <tomek@kownet.info>
2025-11-14 16:59:20 +0100

    final changes

--- apps/user-application/worker/trpc/routers/links.ts
+++ apps/user-application/worker/trpc/routers/links.ts
@@ -4,83 +4,75 @@ import {
   createLinkSchema,
   destinationsSchema,
 } from "@repo/data-ops/zod-schema/links";
+import { activeLinksLastHour, createLink, getLast24And48HourClicks, getLast30DaysClicks, getLast30DaysClicksByCountry, getLink, getLinks, totalLinkClickLastHour, updateLinkDestinations, updateLinkName } from "@repo/data-ops/queries/links";
 
 import { TRPCError } from "@trpc/server";
-import {
-  ACTIVE_LINKS_LAST_HOUR,
-  LAST_30_DAYS_BY_COUNTRY,
-} from "./dummy-data";
 
-import { createLink, getLinks, updateLinkName, getLink, updateLinkDestinations } from "@repo/data-ops/queries/links";
 
 export const linksTrpcRoutes = t.router({
   linkList: t.procedure
-  .input(
-    z.object({
-      offset: z.number().optional(),
-    }),
-  )
-  .query(async ({ctx, input}) => {
+    .input(
+      z.object({
+        offset: z.number().optional(),
+      }),
+    )
+    .query(async ({ctx, input}) => {
 
-    return await getLinks(ctx.userInfo.userId, input.offset?.toString())
-  }),
-createLink: t.procedure.input(createLinkSchema).mutation(async ({ctx, input}) => {
-  const linkId = await createLink({
-    accountId: ctx.userInfo.userId,
-    ...input,
-  });
-  return linkId;
-}),
-updateLinkName: t.procedure
-  .input(
-    z.object({
-      linkId: z.string(),
-      name: z.string().min(1).max(300),
+      return await getLinks(ctx.userInfo.userId, input.offset?.toString())
     }),
-  )
-  .mutation(async ({ input }) => {
-    console.log(input.linkId, input.name);
-    await updateLinkName(input.linkId, input.name)
+  createLink: t.procedure.input(createLinkSchema).mutation(async ({ctx, input}) => {
+    const linkId = await createLink({
+      accountId: ctx.userInfo.userId,
+      ...input,
+    });
+    return linkId;
   }),
-getLink: t.procedure
-  .input(
-    z.object({
-      linkId: z.string(),
+  updateLinkName: t.procedure
+    .input(
+      z.object({
+        linkId: z.string(),
+        name: z.string().min(1).max(300),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      console.log(input.linkId, input.name);
+      await updateLinkName(input.linkId, input.name)
     }),
-  )
-  .query(async ({input}) => {
-    const data = await getLink(input.linkId)
-
-    if (!data) throw new TRPCError({ code: "NOT_FOUND" });
-    return data;
-  }),
-updateLinkDestinations: t.procedure
-  .input(
-    z.object({
-      linkId: z.string(),
-      destinations: destinationsSchema,
+  getLink: t.procedure
+    .input(
+      z.object({
+        linkId: z.string(),
+      }),
+    )
+    .query(async ({input}) => {
+      const data = await getLink(input.linkId)
+ 
+      if (!data) throw new TRPCError({ code: "NOT_FOUND" });
+      return data;
     }),
-  )
-  .mutation(async ({ input }) => {
-    await updateLinkDestinations(input.linkId, input.destinations)
+  updateLinkDestinations: t.procedure
+    .input(
+      z.object({
+        linkId: z.string(),
+        destinations: destinationsSchema,
+      }),
+    )
+    .mutation(async ({ input }) => {
+      await updateLinkDestinations(input.linkId, input.destinations)
+    }),
+  activeLinks: t.procedure.query(async ({ctx}) => {
+    return activeLinksLastHour(ctx.userInfo.userId);
   }),
-  activeLinks: t.procedure.query(async () => {
-    return ACTIVE_LINKS_LAST_HOUR;
+  totalLinkClickLastHour: t.procedure.query(async ({ ctx }) => {
+    return await totalLinkClickLastHour(ctx.userInfo.userId);
   }),
-  totalLinkClickLastHour: t.procedure.query(async () => {
-    return 13;
+  last24HourClicks: t.procedure.query(async ({ ctx }) => {
+    return await getLast24And48HourClicks(ctx.userInfo.userId);
   }),
-  last24HourClicks: t.procedure.query(async () => {
-    return {
-      last24Hours: 56,
-      previous24Hours: 532,
-      percentChange: 12,
-    };
+  last30DaysClicks: t.procedure.query(async ({ ctx }) => {
+    return await getLast30DaysClicks(ctx.userInfo.userId);
   }),
-  last30DaysClicks: t.procedure.query(async () => {
-    return 78;
-  }),
-  clicksByCountry: t.procedure.query(async () => {
-    return LAST_30_DAYS_BY_COUNTRY;
+  clicksByCountry: t.procedure.query(async ({ ctx }) => {
+    return await getLast30DaysClicksByCountry(ctx.userInfo.userId);
   }),
 });
